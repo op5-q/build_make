2cc9555b192f955aaf4c401d039ea65fa5e3b8a3
jhenrique09 <jhenrique09.mcz@hotmail.com>
2019-09-06 21:16:47 -0300

    build: Remove some apps by default
    Change-Id: Ic1478ccfbb88f8cae49f18d51540a85d24a6f9ac

--- target/product/mainline.mk
+++ target/product/mainline.mk
@@ -31,9 +31,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.com.android.dataroaming=true \
 endif
 
-PRODUCT_PACKAGES += \
-    WallpaperPicker \
-
 ifeq ($(AOSIP_BUILD),)
 PRODUCT_COPY_FILES += device/sample/etc/apns-full-conf.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/apns-conf.xml
 endif
